f857a6d5904a7d4faa9eac014e978d04f9f097a4
Ridwan Jokanola <ade@Ridwans-MacBook-Pro.local>
2025-12-23 13:56:41 +0100

    revert: Hardhat ESM compatibility debugging changes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,13 +2,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-viem";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     arc_testnet: {
       url: "https://rpc.testnet.arc.network", // Replace with the actual RPC URL
-      accounts: [process.env.ARC_PRIVATE_KEY as string] // Use environment variable for private key
+      accounts: [process.env.ARC_PRIVATE_KEY], // Use environment variable for private key
+      type: "http"
     }
   }
 };
